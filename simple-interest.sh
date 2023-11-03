8c3a943b6fdcc59244ee6b459ab61e21a4fb3eb3
Mian Hamid Ur Rehman <148252210+Hamid6426@users.noreply.github.com>
2023-11-03 15:59:43 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Hamid6426
    # Input:
    # p, principal amount
    # t, time period in years
